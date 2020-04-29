7139fd0c5f232aa1edddedfb123003979f764ffb
Chase Wiseman <chase@skyverge.com>
2020-04-29 14:28:09 -0700

    Add script localization

--- src/Admin/Emails.php
+++ src/Admin/Emails.php
@@ -96,6 +96,9 @@ final class Emails {
 
 			wp_enqueue_script( 'sv-wc-jilt-prompt-email-scripts', Package::get_assets_url() . '/admin/js/emails.js', [ 'jquery' ], Package::VERSION );
 
+			wp_localize_script( 'sv-wc-jilt-prompt-email-scripts', 'sv_wc_jilt_email_prompt', [
+			] );
+
 		} );
 	}
 
