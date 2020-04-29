da91df2f145d8e9f2bf2049b040ec064d71e933f
Chase Wiseman <chase@skyverge.com>
2020-04-29 15:35:13 -0700

    Add proper PHP file headers

--- src/Admin/Emails.php
+++ src/Admin/Emails.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * Jilt for WooCommerce Promotions
+ *
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@skyverge.com so we can send you a copy immediately.
+ *
+ * @author    SkyVerge
+ * @copyright Copyright (c) 2020, SkyVerge, Inc. (info@skyverge.com)
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
 
 namespace SkyVerge\WooCommerce\Jilt_Promotions\Admin;
+
+defined( 'ABSPATH' ) or exit;
 
 use SkyVerge\WooCommerce\Jilt_Promotions\Package;
 
--- src/Package.php
+++ src/Package.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * Jilt for WooCommerce Promotions
+ *
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@skyverge.com so we can send you a copy immediately.
+ *
+ * @author    SkyVerge
+ * @copyright Copyright (c) 2020, SkyVerge, Inc. (info@skyverge.com)
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
 
 namespace SkyVerge\WooCommerce\Jilt_Promotions;
+
+defined( 'ABSPATH' ) or exit;
 
 /**
  * The base package class.
@@ -17,7 +34,7 @@ class Package {
 
 
 	/**
-	 * Gets the package asserts URL.
+	 * Gets the package assets URL.
 	 *
 	 * @since 1.0.0
 	 *
