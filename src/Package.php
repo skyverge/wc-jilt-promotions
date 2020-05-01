1cb4fe1067baef576072b45813c5f5b86c957661
Chase Wiseman <chase@skyverge.com>
2020-05-01 09:40:20 -0700

    Load translations

--- src/Package.php
+++ src/Package.php
@@ -45,6 +45,9 @@ class Package {
 	public function __construct() {
 
 		$this->includes();
+
+		// load the translation files
+		add_action( 'init', array( $this, 'load_translations' ) );
 	}
 
 
@@ -58,6 +61,26 @@ class Package {
 		require_once( self::get_package_path() . '/Admin/Emails.php' );
 
 		new Admin\Emails();
+	}
+
+
+	/**
+	 * Loads the translation files.
+	 *
+	 * @internal
+	 *
+	 * @since 1.0.0
+	 */
+	public function load_translations() {
+
+		// user's locale if in the admin for WP 4.7+, or the site locale otherwise
+		$locale = is_admin() && is_callable( 'get_user_locale' ) ? get_user_locale() : get_locale();
+
+		$locale = apply_filters( 'plugin_locale', $locale, 'sv-wc-jilt-promotions' );
+
+		load_textdomain( 'sv-wc-jilt-promotions', WP_LANG_DIR . '/sv-wc-jilt-promotions/sv-wc-jilt-promotions-' . $locale . '.mo' );
+
+		load_plugin_textdomain( 'sv-wc-jilt-promotions', false, untrailingslashit( dirname( plugin_basename( __FILE__ ) ) ) . '/i18n/languages' );
 	}
 
 
