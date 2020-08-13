8c9bb61ff5292fa03ec4f87bd064c421bf242b36
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-08-13 12:27:12 +0200

    Add the InstallPluginModal class

--- src/assets/js/admin/promotions.js
+++ src/assets/js/admin/promotions.js
@@ -1,3 +1,46 @@
-jQuery( document ).ready( function( $ ) {
+jQuery( function( $ ) {
+
+
+	/**
+	 * Jilt Plugin Installation modal handler.
+	 *
+	 * @since 1.1.0-dev.1
+	 */
+	$.JiltPromotions = class InstallPluginModal {
+
+
+		/**
+		 * Constructor.
+		 *
+		 * @since 1.1.0-dev.1
+		 *
+		 * @param {object} options
+		 */
+		constructor( options ) {
+
+		}
+
+
+		/**
+		 * Initializes modal.
+		 *
+		 * @since 1.1.0-dev.1
+		 */
+		initialize() {
+
+		}
+
+
+		/**
+		 * Opens the modal.
+		 *
+		 * @since 1.1.0-dev.1
+		 */
+		open() {
+
+		}
+
+
+	}
 
 } );
