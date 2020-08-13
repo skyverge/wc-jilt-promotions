80bb247b45c98e74c2ceb385ea08385143718209
Nabeel Molham <n.molham@gmail.com>
2020-08-13 19:55:02 +0200

    Include and initialize Product prompt

--- src/Package.php
+++ src/Package.php
@@ -74,11 +74,13 @@ class Package {
 		require_once( self::get_package_path() . '/Admin/Customers.php' );
 		require_once( self::get_package_path() . '/Admin/Emails.php' );
 		require_once( self::get_package_path() . '/Admin/Orders.php' );
+		require_once( self::get_package_path() . '/Admin/Product.php' );
 
 		new Messages();
 		new Handlers\Installation();
 		new Admin\Customers();
 		new Admin\Emails();
+		new Admin\Product();
 	}
 
 
