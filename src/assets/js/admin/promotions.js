07d5740044b33ba6971ad3ef2143f08673975865
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-08-13 14:32:31 +0200

    Call static methods

--- src/assets/js/admin/promotions.js
+++ src/assets/js/admin/promotions.js
@@ -168,7 +168,7 @@ jQuery( function( $ ) {
 		 * @param {string} messageID message identifier
 		 */
 		static enableMessage( messageID ) {
-			$.JiltPromotions.Messages.handleMessage( messageID, 'sv_wc_jilt_enable_message', 'sv_wc_jilt_prompt_install.nonces.enable_message' )
+			Messages.handleMessage( messageID, 'sv_wc_jilt_enable_message', 'sv_wc_jilt_prompt_install.nonces.enable_message' )
 		}
 
 
@@ -180,7 +180,7 @@ jQuery( function( $ ) {
 		 * @param {string} messageID message identifier
 		 */
 		static dismissMessage( messageID ) {
-			$.JiltPromotions.Messages.handleMessage( messageID, 'sv_wc_jilt_dismiss_message', 'sv_wc_jilt_prompt_install.nonces.dismiss_message' )
+			Messages.handleMessage( messageID, 'sv_wc_jilt_dismiss_message', 'sv_wc_jilt_prompt_install.nonces.dismiss_message' )
 		}
 
 
