2d1cb56e764842b3b800f884c9462b834054107c
Adriano Castro <adrianostanley@gmail.com>
2020-08-14 17:29:38 -0300

    Orders: remove the last pipe after removing the Abandoned Carts view tab

--- src/assets/js/admin/orders.js
+++ src/assets/js/admin/orders.js
@@ -23,7 +23,21 @@ jQuery( document ).ready( function ($) {
 
 				$.JiltPromotions.Messages.dismissMessage( sv_wc_jilt_prompt_orders.abandoned_carts_id );
 
+				// the parent will be used to remove the last pipe '|' after removing the view
+				const viewParent = $( `.${modalClass}` ).parent();
+
+				// removes the view link from the views menu
 				$( `.${modalClass}` ).remove();
+
+				const lastView = viewParent.find( 'li:last-child' );
+
+				if ( lastView.length === 1 ) {
+
+					const lastViewHtml = lastView.html();
+
+					// removes the last pipe after removing the view
+					lastView.html( lastViewHtml.replace( '|', '' ) )
+				}
 			} );
 
 			shouldDisplayModal = false;
