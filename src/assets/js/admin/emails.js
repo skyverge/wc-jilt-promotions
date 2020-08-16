3b7390b15e220cbda597c864294a2dea58880cb6
Nabeel Molham <n.molham@gmail.com>
2020-08-16 17:22:11 +0200

    Remove the deprecated Emails install CTA click handler

--- src/assets/js/admin/emails.js
+++ src/assets/js/admin/emails.js
@@ -13,56 +13,6 @@ jQuery( document ).ready( function( $ ) {
 	} );
 
 
-	// handle the modal "install" button click
-	$( document ).on( 'click', '#sv-wc-jilt-install-button-install', function( event ) {
-
-		event.preventDefault();
-
-		$( '#sv-wc-jilt-install-modal .wc-backbone-modal-content' ).block( {
-			message: null,
-			overlayCSS: {
-				background: '#fff',
-				opacity: 0.6
-			}
-		} );
-
-		$.post(
-			ajaxurl,
-			{
-				action:   'sv_wc_jilt_install_jilt',
-				nonce:    sv_wc_jilt_email_prompt.nonces.install_plugin,
-				email_id: sv_wc_jilt_email_prompt.email_id,
-			}
-		).then( function( response ) {
-
-			if ( response.success && response.data.redirect_url ) {
-
-				window.location = response.data.redirect_url;
-
-			} else {
-
-				console.error( response );
-
-				$( '#sv-wc-jilt-install-modal article' ).html( sv_wc_jilt_email_prompt.i18n.install_error );
-
-				$( '#sv-wc-jilt-install-button-install' ).hide();
-			}
-
-		} ).fail( function() {
-
-			$( '#sv-wc-jilt-install-modal article' ).html( sv_wc_jilt_email_prompt.i18n.install_error );
-
-			$( '#sv-wc-jilt-install-button-install' ).hide();
-
-		} ).always( function() {
-
-			$( '#sv-wc-jilt-install-modal .wc-backbone-modal-content' ).unblock();
-
-		} );
-
-	} );
-
-
 	// handle the "hide" CTA
 	$( '#sv-wc-jilt-emails-install-prompt .sv-wc-jilt-prompt-hide-cta' ).click( function( event ) {
 
